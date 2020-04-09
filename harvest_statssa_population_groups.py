f5b7e6f2bb92281ec5dbb10a57908b7c7c212322
Kusasa <skusasalethu@gmail.com>
2020-04-09 13:48:15 +0200

    edited interface design and added data dropdown option

--- harvest_statssa_population_groups.py
+++ harvest_statssa_population_groups.py
@@ -17,32 +17,44 @@ import tkinter as tk
 #Funtion for data downloading and processing
 def processor():
     ws = input_wd.get()
+    target_data = dropdown.get()
+    
+    #Data Options
+    data_options = {
+            "rsa_population_groups": ['http://www.statssa.gov.za/wp-content/themes/umkhanyakude/mapajax.php', '/rsa_population_groups.shp'],
+            "two": ["url", "/file_name.shp"],
+            "three": ["ur", "/file_name.shp"]
+    }
     
     #Download data from statsa url
     params = (
         ('shape', 'za.geojson'),
     )
     
-    response = requests.get('http://www.statssa.gov.za/wp-content/themes/umkhanyakude/mapajax.php', params=params)
+    response = requests.get(data_options[target_data][0], params=params)
     
     # Save geojson data on disk as a shapefile
     response_text = response.text
     json = gpd.read_file(response_text)
-    json.to_file(ws + "/rsa_population_groups.shp")
+    json.to_file(ws + data_options[target_data][1])
 
 # Tkinter Interactions
 def main_screen():
     window = tk.Tk()
     window.title("StatsSA Population Groups Data Harvester")
-    window.geometry("500x50")
+    window.geometry("760x60")
     
-    global input_wd
+    global input_wd, dropdown
     input_wd = tk.StringVar()
+    dropdown = tk.StringVar()
+    dropdown.set("rsa_population_groups") # default value
     
-    tk.Label(window, text="Please enter details below").pack()
+    tk.Label(window, text="Please enter details below").pack(side = tk.TOP)
     tk.Label(window, text="Working Directory: ").pack(side = tk.LEFT)
     tk.Entry(window, bd =5, width = 50, textvariable = input_wd).pack(side = tk.LEFT)
-    tk.Button(text="Run",command = processor ).pack()
+    tk.Label(window, text="Target Data: ").pack(side = tk.LEFT)
+    tk.OptionMenu(window, dropdown, "rsa_population_groups", "two", "three").pack(side = tk.LEFT)
+    tk.Button(text="Run",command = processor, width=10).pack(side = tk.RIGHT)
     window.mainloop()
     
 main_screen()
